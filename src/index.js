ca00a02c926f7cbaa2a9f8a197f5da1b267fa965
Lassi Aaltonen <lassiaa@k-d557-mac-31.pk.amk.root>
2023-01-13 14:42:29 +0200

    Done

--- src/index.js
+++ src/index.js
@@ -15,46 +15,44 @@ const coursesFi = ["Jauhelihapihvi, ruskeaa kermakastiketta ja keitettyä peruna
 let lang = 'fi';
 let activeMenu = coursesFi;
 
+let menuOrder = 'asc';
+
 /**
  * Renders menu content to html page
  * @param {Array} menu - Array of dishes
  */
-
 const renderMenu = (menu) => {
-  const menuBox = document.querySelector('.restaurant1');
+  const menuBox = document.querySelector('.restaurant');
   menuBox.innerHTML = '';
   const list = document.createElement('ul');
-
   for (const dish of menu) {
     const li = document.createElement('li');
     li.textContent = dish;
     list.append(li);
-  };
-  menuBox.append.list;
+  }
+  menuBox.append(list);
 };
 
 renderMenu(activeMenu);
 
 /**
- * Sorts menu alphabetically
+ * Sorts menu alphapetically
  * @param {Array} menu - Array of dishes
- * @param {String} order - 'asc' or 'desc'
- * @returns
+ * @param {string} order - 'asc' or 'desc'
+ * @returns sorted menu array
  */
-
 const sortMenu = (menu, order='asc') => {
   menu.sort();
-  if (order === 'desc'){
+  if (order === 'desc') {
     menu.reverse();
-  };
+  }
   return menu;
 };
 
 /**
  * Change UI language
- * @param {String} language
+ * @param {string} language
  */
-
 const changeLanguage = (language) => {
   if (language === 'fi') {
     activeMenu = coursesFi;
@@ -66,7 +64,7 @@ const changeLanguage = (language) => {
 };
 
 /**
- * Returns a random dish from an array
+ * Get a random dish from menu
  * @param {Array} menu - Array of dishes
  * @returns random dish item
  */
@@ -75,8 +73,30 @@ const getRandomDish = (menu) => {
   return menu[randomIndex];
 };
 
-
 const sortButton = document.querySelector('#sort-button');
 sortButton.addEventListener('click', () => {
-  renderMenu(sortMenu(activeMenu));
+  console.log(menuOrder);
+  if (menuOrder === 'asc') {
+    menuOrder = 'desc';
+  } else if (menuOrder === 'desc') {
+    menuOrder = 'asc';
+  }
+  console.log(menuOrder);
+  renderMenu(sortMenu(activeMenu, menuOrder));
+});
+
+const langButton = document.querySelector('#lang-button');
+langButton.addEventListener('click', () => {
+  if (lang === 'fi') {
+    lang = 'en';
+  } else {
+    lang = 'fi';
+  };
+  renderMenu(activeMenu, changeLanguage(lang));
+});
+
+const randomButton = document.querySelector('#random-button');
+const randomDisplay = document.querySelector('#random-display');
+randomButton.addEventListener('click', () => {
+  randomDisplay.textContent = getRandomDish(activeMenu);
 });
--- webpack.common.js
+++ webpack.common.js
@@ -19,7 +19,7 @@ module.exports = {
       },
     ]}),
     new HtmlWebpackPlugin({
-      title: 'WTMP Starter',
+      title: 'Week1-Task3',
       meta: {
         viewport: 'width=device-width, initial-scale=1.0'
       },
